c4d1a3325ab711e754c072cd39455604092c242b
liuxinghong <349695133@qq.com>
2019-05-10 11:16:59 +0800

    集成oauth2和token

--- src/main/java/com/example/demo/config/SwaggerConfiguration.java
+++ src/main/java/com/example/demo/config/SwaggerConfiguration.java
@@ -1,15 +1,22 @@
 package com.example.demo.config;
 
+import com.google.common.collect.Sets;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author liuxinghong
@@ -21,12 +28,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket createRestApi() {
+    public Docket configSpringfoxDocketForAll() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
+                .produces(Sets.newHashSet("application/json","application/x-www-form-urlencoded","*/*"))
+                .consumes(Sets.newHashSet("application/json","application/x-www-form-urlencoded","*/*"))
+                .protocols(Sets.newHashSet("http"))
+                .forCodeGeneration(true)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
                 .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
                 .build()
+                .globalOperationParameters(pars)
                 .apiInfo(apiInfo());
     }
     private ApiInfo apiInfo(){
